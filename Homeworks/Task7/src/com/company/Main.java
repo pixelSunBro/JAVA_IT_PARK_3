0f3d14f1b0bb43b688a6706003b396c44b49829b
pixelSunBro <pixelsunbro@gmail.com>
2017-10-22 16:22:21 +0300

    Task7

--- Homeworks/Task7/src/com/company/Main.java
+++ Homeworks/Task7/src/com/company/Main.java
@@ -7,20 +7,8 @@ public class Main {
         Ellipse ellipse = new Ellipse(7, 3);
         Triangle triangle = new Triangle(2, 9, 4, 7);
 
-        /*System.out.println("Периметр прямоугольника и квадрата: " + squareRectangle.calcPerimeterSquareRectangle());
-        System.out.println("Площадь прямоугольника и квадрата: " + squareRectangle.calcSquareSquareRectangle());
-
-        System.out.println("Периметр круга: " + circle.calcPerimeterCircle());
-        System.out.println("Площадь круга: " + circle.calcSquareCircle());
-
-        System.out.println("Периметр овала: " + ellipse.calcPerimetereEllipse());
-        System.out.println("Площадь овала: " + ellipse.calcSquareEllipse());
-
-        System.out.println("Периметр треугольника: " + triangle.calcPerimetereTriangle());
-        System.out.println("Площадь треугольника: " + triangle.calcSquareTriangle());*/
-
-         Figure figure[] = {squareRectangle, circle, ellipse, triangle};
-        for(int i = 0; i < figure.length; i++) {
+        Figure figure[] = {squareRectangle, circle, ellipse, triangle};
+        for (int i = 0; i < figure.length; i++) {
             figure[i].calculateArea();
             figure[i].calculatePerimeter();
         }
